78d1edafa100babe811044cf225e6f6e2a2482ad
tchovi <79668896+tchovi@users.noreply.github.com>
2025-07-17 22:07:15 -0400

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'icecysoap');
+define('DB_USER', 'icecy_user');
+define('DB_PASS', 'secure_password');
+define('MUSIC_DIR', '/var/lib/icecysoap/music');
+define('BASE_URL', 'http://localhost:8088');
